e3e21eeae4348566a68bc70290197d3f76e0cfeb
Rishi Karanam <rshkrnm@gmail.com>
2024-02-16 22:42:41 +0530

    cors mod ag

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const CORSMiddleware = require("./CORS.js")
 const app = express();
 
 app.use(CORSMiddleware);
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 
 app.get("/",(req,res)=>{
