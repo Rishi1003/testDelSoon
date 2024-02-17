f218ab1713c0247cb2d0358e8d6b5efcdd238beb
Rishi Karanam <rshkrnm@gmail.com>
2024-02-17 12:16:09 +0530

    asfd

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const jwt = require('jsonwebtoken');
 // const cors = require("cors")
-const CORSMiddleware = require("./CORS.js")
+// const CORSMiddleware = require("./CORS.js")
 
 const app = express();
 
-app.use(CORSMiddleware);
+// app.use(CORSMiddleware);
 // app.use(cors());
 
 app.use(cookieParser());
 
 app.get("/",(req,res)=>{
     console.log("hits");
-    const token = jwt.sign("safd", "hellosecrestsfd");
-    res.send(`${token}`);
+    console.log(req.headers);
 });
 
 app.post("/",(req,res)=>{
